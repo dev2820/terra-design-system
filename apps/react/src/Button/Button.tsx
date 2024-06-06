86c45066742c01cef30d9e1e1a0bc1f00ad4004b
dev2820 <second28200@naver.com>
2024-06-06 17:13:59 +0900

    feat(wip): Button 컴포넌트 구현중...

--- apps/react/src/Button/Button.tsx
+++ apps/react/src/Button/Button.tsx
@@ -1,22 +1,89 @@
-import type { ComponentProps } from 'react';
+import { LoaderCircleIcon } from 'lucide-react';
 
-import { css } from '../../styled-system/css';
+import type { ComponentProps, ReactNode } from 'react';
 
-export type ButtonProps = ComponentProps<'button'>;
+import { cx, cva, type RecipeVariantProps, css } from '../../styled-system/css';
 
-export const Button = ({ children }: ButtonProps) => {
+const buttonVariants = cva({
+  base: {
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    whiteSpace: 'nowrap',
+    rounded: 'md',
+    fontSize: 'md',
+    fontWeight: 'medium',
+    gap: '1',
+    transition: ['colors'],
+    _hover: {},
+    _active: {},
+  },
+  variants: {
+    variant: {
+      filled: {},
+      outline: {},
+      ghost: {},
+    },
+    color: {
+      primary: {},
+      secondary: {},
+    },
+    size: {
+      lg: {},
+      md: {},
+      sm: {},
+      icon: {},
+    },
+    disabled: {
+      true: {
+        cursor: 'not-allowed',
+      },
+    },
+    loading: {
+      true: {
+        cursor: 'wait',
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'filled',
+    color: 'primary',
+    size: 'md',
+    disabled: false,
+  },
+  compoundVariants: [],
+});
+
+export type ButtonProps = ComponentProps<'button'> &
+  RecipeVariantProps<typeof buttonVariants> & {
+    loading: boolean;
+    loadingIcon?: ReactNode;
+    rightIcon?: ReactNode;
+    leftIcon?: ReactNode;
+  };
+
+export function Button({
+  type = 'button',
+  loading = false,
+  loadingIcon = (
+    <LoaderCircleIcon className={css({ animation: 'spin' })} size={16} />
+  ),
+  leftIcon,
+  rightIcon,
+  disabled,
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className={css({
-        bg: 'red.300',
-        fontFamily: 'Inter',
-        px: '4',
-        py: '3',
-        borderRadius: 'md',
-        _hover: { bg: 'red.400' },
-      })}
+      className={cx(buttonVariants({ disabled, loading }))}
+      type={type}
+      {...props}
     >
+      {leftIcon}
+      {loading && loadingIcon}
       {children}
+      {rightIcon}
     </button>
   );
-};
+}
