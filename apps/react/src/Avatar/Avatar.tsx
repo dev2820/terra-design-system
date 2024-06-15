d2bb31cbcecc4904d8626eacc353eed43432d4f8
dev2820 <second28200@naver.com>
2024-06-15 23:20:05 +0900

    feat(wip): Avatar 뼈대 생성
    - props를 뭘 열지 고민

--- apps/react/src/Avatar/Avatar.tsx
+++ apps/react/src/Avatar/Avatar.tsx
@@ -1,1 +1,101 @@
-//
+import { avatarAnatomy } from '@ark-ui/anatomy';
+import { Avatar as _Avatar } from '@ark-ui/react';
+import { UserIcon } from 'lucide-react';
+
+import {
+  type ComponentProps,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+  forwardRef,
+} from 'react';
+
+import { RecipeVariantProps, cx, sva } from '../../styled-system/css';
+
+export const avatarVariants = sva({
+  className: 'avatar',
+  slots: avatarAnatomy.keys(),
+  base: {
+    root: {
+      rounded: 'full',
+      overflow: 'hidden',
+    },
+    fallback: {
+      bg: 'neutral.500',
+    },
+    image: {
+      bg: 'neutral.500',
+    },
+  },
+  variants: {
+    size: {
+      sm: {
+        root: {
+          w: 8,
+          h: 8,
+        },
+        fallback: {
+          w: 8,
+          h: 8,
+        },
+        image: {
+          w: 8,
+          h: 8,
+        },
+      },
+      md: {
+        root: {
+          w: 10,
+          h: 10,
+        },
+        fallback: {
+          w: 10,
+          h: 10,
+        },
+        image: {
+          w: 10,
+          h: 10,
+        },
+      },
+      lg: {
+        root: {
+          w: 12,
+          h: 12,
+        },
+        fallback: {
+          w: 12,
+          h: 12,
+        },
+        image: {
+          w: 12,
+          h: 12,
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+  },
+});
+
+export type AvatarProps = ComponentProps<typeof Avatar>;
+const Avatar = forwardRef<
+  ElementRef<typeof _Avatar.Root>,
+  ComponentPropsWithoutRef<typeof _Avatar.Root> &
+    RecipeVariantProps<typeof avatarVariants> & {
+      alt?: string;
+      src?: string;
+    }
+>(({ src, alt, size, className, ...props }, ref) => {
+  const classes = avatarVariants({ size });
+
+  return (
+    <_Avatar.Root ref={ref} className={cx(classes.root, className)} {...props}>
+      <_Avatar.Image src={src} alt={alt} className={classes.image} />
+      <_Avatar.Fallback className={classes.fallback}>
+        <UserIcon size={size} />
+      </_Avatar.Fallback>
+    </_Avatar.Root>
+  );
+});
+
+export { Avatar };
--- apps/react/src/Avatar/_Avatar.stories.tsx
+++ apps/react/src/Avatar/_Avatar.stories.tsx
@@ -1,1 +1,45 @@
-//
+import { cx } from '../../styled-system/css';
+import { flex } from '../../styled-system/patterns';
+import { Avatar, type AvatarProps } from './index';
+
+/**
+ * based on https://ark-ui.com/react/docs/components/avatar
+ */
+export default {
+  title: 'Components/Avatar',
+  component: Avatar,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+};
+
+export const Default = {
+  args: {
+    alt: 'pravatar',
+    src: 'https://i.pravatar.cc/500',
+    size: 'md',
+  },
+  render: (props: AvatarProps) => {
+    return <Avatar {...props} />;
+  },
+};
+
+export const Size = {
+  args: {
+    alt: 'pravatar',
+    src: 'https://i.pravatar.cc/500',
+  },
+  render: (props: AvatarProps) => {
+    const sizes = ['sm', 'md', 'lg'] as AvatarProps['size'][];
+    return (
+      <div className={cx(flex({ direction: 'row', gap: 4 }))}>
+        {sizes.map(size => (
+          <Avatar size={size} {...props} />
+        ))}
+      </div>
+    );
+  },
+};
