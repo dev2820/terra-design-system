407228520888639ddb6a59791a37cdd5d982c6ca
dev2820 <dev2820@users.noreply.github.com>
2024-08-16 08:39:51 +0000

    deploy: acd26b7d7a9c07188df97246d750de9dd3bdb4c1

---
+++ assets/_Card.stories-BGg61pQG.js
@@ -0,0 +1,59 @@
+import{j as r}from"./jsx-runtime-qGIIFXMu.js";import{a as e,c as g}from"./cva-C095pwBQ.js";import{f}from"./spacer-CRhrBJcD.js";import"./Accordion-Da7P7QzH.js";import"./Alert-D8VVORGz.js";import{A as y}from"./Avatar-jKtYeNtN.js";import"./Badge-cQvqgMYI.js";import{B as s}from"./Button-BVDRYxdW.js";import{C as t}from"./Skeleton-DDfGwKNX.js";import"./Carousel-DxP1P1RQ.js";import"./Checkbox-BWMCbP_A.js";import"./Collapsible-BbSz4189.js";import"./ColorPicker-XXgAMpev.js";import"./Combobox-BBajB62v.js";import"./DatePicker-C1doOxIT.js";import"./Dialog-D2Zzl4XO.js";import"./Drawer-NDdzcvDJ.js";import"./Field-nBX6hYKG.js";import"./FileUpload-TzBguohA.js";import"./IconButton-E43GEZXc.js";import"./Input-BydU8ZSw.js";import"./Link-CuuQqiTV.js";import"./NumberInput-RPtPcVdT.js";import"./Pagination-BIVaeef8.js";import"./Popover-5rOsQBqx.js";import"./Progress-Dh_hq_qi.js";import"./RadioGroup-DXIi5zCg.js";import"./RatingGroup-RORko1Gz.js";import"./SegmentGroup-CK7ggxCb.js";import"./Select-Dd1BRsgE.js";import"./Slider-Bh_7WDID.js";import"./Spinner-BE0ZfWCW.js";import"./Switch-DMJDMDOm.js";import"./Table-QG2pYdBk.js";import"./Tabs-DzINcWS2.js";import"./Textarea-CCfh2OzW.js";import"./Toast-B085qfZz.js";import"./Tooltip-DZ-9l1tq.js";import"./TreeView-Cf212LSZ.js";import"./index-CDs2tPxN.js";import"./sva-CxSEfBjn.js";import"./create-react-context-Dw26gLIP.js";import"./factory-BCwUXDVF.js";import"./index-B-yFm4aN.js";import"./index-Dquv0SIZ.js";import"./index-sV3Drpq0.js";import"./index-DJ3ti8gJ.js";import"./chevron-down-Du6SRUSG.js";import"./createLucideIcon-zijr-BYg.js";import"./create-split-props-u5h9OPvL.js";import"./render-strategy-B0fJl-aJ.js";import"./index-qa1jsk-d.js";import"./use-locale-context-0tfyuFre.js";import"./use-event-eGuf5zsd.js";import"./collapsible-content-DPpUoDh8.js";import"./index-DOljKnny.js";import"./check-DUDaO23Y.js";import"./index-DBzJ7FiP.js";import"./use-field-context-BjqdMh-O.js";import"./index-4KpmSbEW.js";import"./split-presence-props-DcTE3k_W.js";import"./use-presence-zw3amx30.js";import"./index-DDey0k43.js";import"./index-Bi-kwPO5.js";import"./portal-DVHZYSwj.js";import"./index-BI_J112u.js";import"./index-B-4z8KQ4.js";import"./dialog-trigger-DxUI0aph.js";import"./index-CgflYHCK.js";import"./chevron-left-Bqxy6nNn.js";import"./ellipsis-Bl27Sm0D.js";import"./chevron-right-CT14g65n.js";import"./index-VEgPwRzq.js";import"./index-BR8t6gqn.js";import"./index-hTcMZcOQ.js";const C=["elevated","outline","filled"],Vr={title:"Components/Card",component:t.Root,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{options:C,control:{type:"select"}}},args:{}},o={args:{},render:i=>r.jsxs(t.Root,{...i,children:[r.jsxs(t.Header,{children:[r.jsx(t.Title,{children:"Sign in"}),r.jsx(t.Description,{children:"Welcome! Begin your journey here."})]}),r.jsxs(t.Content,{className:e({display:"flex",flexDir:"column"}),children:[r.jsx("input",{type:"text",placeholder:"your email"}),r.jsx("input",{type:"password",placeholder:"your password"})]}),r.jsxs(t.Footer,{className:e({display:"flex",flexDir:"row",gap:4}),children:[r.jsx(s,{className:e({flex:1}),children:"Sign in"}),r.jsx(s,{theme:"primary",className:e({flex:1}),children:"🚀 Sign up"})]})]})},n={args:{},render:i=>r.jsx("div",{className:g(f({direction:"row",gap:4})),children:C.map(a=>r.jsxs(t.Root,{variant:a,...i,children:[r.jsx(t.Header,{children:r.jsx(y,{src:"https://i.pravatar.cc/500",size:"lg"})}),r.jsx(t.Content,{children:"An adventurer at heart and a visionary by nature, Alex has journeyed through the unknown, bringing light to the darkest corners of the world."}),r.jsx(t.Footer,{children:r.jsx("span",{className:e({fontSize:"md",fontWeight:"bold"}),children:"- Meet Alex Winters"})})]},a))})};var p,m,d,l,c;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {},
+  render: (props: CardProps) => {
+    return <Card.Root {...props}>
+        <Card.Header>
+          <Card.Title>Sign in</Card.Title>
+          <Card.Description>Welcome! Begin your journey here.</Card.Description>
+        </Card.Header>
+        <Card.Content className={css({
+        display: 'flex',
+        flexDir: 'column'
+      })}>
+          <input type="text" placeholder="your email" />
+          <input type="password" placeholder="your password" />
+        </Card.Content>
+        <Card.Footer className={css({
+        display: 'flex',
+        flexDir: 'row',
+        gap: 4
+      })}>
+          <Button className={css({
+          flex: 1
+        })}>Sign in</Button>
+          <Button theme="primary" className={css({
+          flex: 1
+        })}>
+            🚀 Sign up
+          </Button>
+        </Card.Footer>
+      </Card.Root>;
+  }
+}`,...(d=(m=o.parameters)==null?void 0:m.docs)==null?void 0:d.source},description:{story:"TODO: 추후 Input component로 교체 필요",...(c=(l=o.parameters)==null?void 0:l.docs)==null?void 0:c.description}}};var u,x,h;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {},
+  render: (props: CardProps) => {
+    return <div className={cx(flex({
+      direction: 'row',
+      gap: 4
+    }))}>
+        {variants.map(variant => <Card.Root variant={variant} key={variant} {...props}>
+            <Card.Header>
+              <Avatar src="https://i.pravatar.cc/500" size="lg" />
+            </Card.Header>
+            <Card.Content>
+              An adventurer at heart and a visionary by nature, Alex has
+              journeyed through the unknown, bringing light to the darkest
+              corners of the world.
+            </Card.Content>
+            <Card.Footer>
+              <span className={css({
+            fontSize: 'md',
+            fontWeight: 'bold'
+          })}>
+                - Meet Alex Winters
+              </span>
+            </Card.Footer>
+          </Card.Root>)}
+      </div>;
+  }
+}`,...(h=(x=n.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};const Ir=["Default","Variant"];export{o as Default,n as Variant,Ir as __namedExportsOrder,Vr as default};
