cc8d6c352bd5ba7693413f2de5bd1a45263e2f80
dev2820 <second28200@naver.com>
2024-09-02 00:05:52 +0900

    feat: input의 outline 두께를 줄임

--- apps/react/src/Input/Input.tsx
+++ apps/react/src/Input/Input.tsx
@@ -12,7 +12,7 @@ const inputVariants = tv({
     'file:trds-border-0 file:trds-bg-transparent file:trds-text-sm file:trds-font-medium',
     'placeholder:trds-text-fg-placeholder',
     'disabled:trds-cursor-not-allowed disabled:trds-opacity-50',
-    'focus-within:trds-border-primary-500 focus-within:trds-shadow-[0_0_0_2px_var(--shadow-color)] focus-within:trds-shadow-primary-500',
+    'focus-within:trds-border-primary-500 focus-within:trds-shadow-[0_0_0_1px_var(--shadow-color)] focus-within:trds-shadow-primary-500',
   ],
   variants: {
     invalid: {
