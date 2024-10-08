07d9d5100226c161b8c51326a0316516d17cc65b
dev2820 <dev2820@users.noreply.github.com>
2024-10-08 00:56:28 +0000

    deploy: 546306c6062ef6de40d41b56a8c2f38bec26ae34

---
+++ assets/_Pagination.stories-DX8t40dd.js
@@ -0,0 +1,29 @@
+import{j as a}from"./jsx-runtime-qGIIFXMu.js";import{R as t}from"./Pagination-qWKgpWO9.js";import"./index-CDs2tPxN.js";import"./index-9MS2e6do.js";import"./tv-Br1lVx4G.js";import"./IconButton-BViwHdHD.js";import"./Button-D9MpFO6Q.js";import"./createLucideIcon-zijr-BYg.js";import"./Link-3roIaot6.js";import"./chevron-left-Bqxy6nNn.js";import"./ellipsis-Bl27Sm0D.js";import"./chevron-right-CT14g65n.js";import"./factory-BCO0_PXk.js";import"./index-B-yFm4aN.js";import"./create-split-props-u5h9OPvL.js";import"./index-B1LdDssV.js";import"./use-locale-context-CTaGYWMe.js";import"./use-event-eGuf5zsd.js";const v={title:"Components/Pagination",component:t,parameters:{layout:"centered",docs:{description:{component:"based on https://ark-ui.com/react/docs/components/pagination"}}},tags:["autodocs"],argTypes:{},args:{}},e={args:{count:100,pageSize:10,defaultPage:1},render:r=>a.jsx(t,{...r})},o={args:{count:100,pageSize:10,defaultPage:1,siblingCount:2},render:r=>a.jsx(t,{...r})},n={args:{count:100,pageSize:10,defaultPage:1},render:r=>a.jsx(t,{...r,type:"link"})};var s,p,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    count: 100,
+    pageSize: 10,
+    defaultPage: 1
+  },
+  render: (props: PaginationProps) => {
+    return <Pagination {...props} />;
+  }
+}`,...(i=(p=e.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var m,c,u;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    count: 100,
+    pageSize: 10,
+    defaultPage: 1,
+    siblingCount: 2
+  },
+  render: (props: PaginationProps) => {
+    return <Pagination {...props} />;
+  }
+}`,...(u=(c=o.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var g,d,l;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    count: 100,
+    pageSize: 10,
+    defaultPage: 1
+  },
+  render: (props: PaginationProps) => {
+    return <Pagination {...props} type="link" />;
+  }
+}`,...(l=(d=n.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const w=["Default","SibilingCount","Type"];export{e as Default,o as SibilingCount,n as Type,w as __namedExportsOrder,v as default};
