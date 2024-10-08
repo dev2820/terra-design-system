b127424284b9b1ac501bba1afe1c16f1d8990bc5
dev2820 <second28200@naver.com>
2024-10-08 09:53:26 +0900

    fix(link): underline hover effect 제거

--- apps/react/src/Link/Link.tsx
+++ apps/react/src/Link/Link.tsx
@@ -7,17 +7,14 @@ import { tv, VariantProps } from '../tv';
 
 const linkVariants = tv({
   base: [
-    'trds-relative trds-inline-flex trds-items-center trds-justify-start trds-whitespace-nowrap trds-gap-1 trds-transition-colors trds-cursor-pointer',
-    'before:trds-duration-200',
-    'before:trds-content-[""] before:trds-absolute before:trds-bottom-0 before:trds-left-0 before:trds-w-full before:trds-h-0.5 before:trds-opacity-0',
-    'hover:before:trds-opacity-100',
+    'trds-inline-flex trds-items-center trds-justify-start trds-whitespace-nowrap trds-gap-1 trds-transition-colors trds-cursor-pointer trds-underline-offset-2 trds-underline-under trds-decoration-[2px] trds-underline',
   ],
   variants: {
     theme: {
       neutral:
-        'trds-text-neutral-500 active:trds-text-neutral-800 hover:trds-text-neutral-700 before:trds-bg-neutral-700',
+        'trds-text-neutral-500 active:trds-text-neutral-800 hover:trds-text-neutral-700 trds-border-neutral-500',
       primary:
-        'trds-text-primary active:trds-text-primary-pressed hover:trds-text-primary-hover before:trds-bg-primary-hover',
+        'trds-text-primary active:trds-text-primary-pressed hover:trds-text-primary-hover trds-border-primary',
     },
   },
   defaultVariants: {
