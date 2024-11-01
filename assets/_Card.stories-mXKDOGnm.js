d6be6cc58fae809b03059225810b78cdc64f89b0
dev2820 <dev2820@users.noreply.github.com>
2024-11-01 13:21:07 +0000

    deploy: b59f702f3ec69be796f867843398ea8ac1a42a98

---
+++ assets/_Card.stories-mXKDOGnm.js
@@ -0,0 +1,42 @@
+import{j as r}from"./jsx-runtime-qGIIFXMu.js";import"./Accordion-B2vZCjVo.js";import"./Alert-DzdkTwAr.js";import{A as g}from"./Avatar-BROtaPwN.js";import"./Badge-oZjtibBZ.js";import{B as n}from"./Button-D9MpFO6Q.js";import{C as t}from"./Skeleton-C2vvedNN.js";import"./Carousel-BrxOJkld.js";import"./Checkbox-DF66v1-a.js";import"./Collapsible-B8dRQuFX.js";import"./ColorPicker-C8JkBZFp.js";import"./Combobox-B0r7LOya.js";import"./DatePicker-C2G3fegT.js";import"./Dialog-Dp73vICh.js";import"./Drawer-BwUmlvAJ.js";import"./Field-CY0mMguP.js";import"./FileUpload-BVrxc-Td.js";import"./IconButton-BViwHdHD.js";import{I as i}from"./Input-ZtbnXEXq.js";import"./Link-3roIaot6.js";import"./NumberInput-DCxpjDqc.js";import"./Pagination-qWKgpWO9.js";import"./Popover-Cdz__TCN.js";import"./Progress-j8Sem724.js";import"./RadioGroup-CxsUeODP.js";import"./RatingGroup-CJNJph1V.js";import"./SegmentGroup-BU_dCfa4.js";import"./Select-CmNQr0Sh.js";import"./Slider-Bn156c4b.js";import"./Spinner-Cmv6BC9H.js";import"./Switch-CUznHoqj.js";import"./Table-CXsaCy3q.js";import"./Tabs-BpnWyLEn.js";import"./Textarea-BnqGUJbA.js";import"./Toast-C8hrAMoO.js";import"./Tooltip-CcBHVL41.js";import"./TreeView-Cmp6EtpF.js";import"./index-CDs2tPxN.js";import"./create-react-context-Dw26gLIP.js";import"./tv-Br1lVx4G.js";import"./chevron-down-Du6SRUSG.js";import"./createLucideIcon-zijr-BYg.js";import"./factory-BCO0_PXk.js";import"./index-B-yFm4aN.js";import"./create-split-props-u5h9OPvL.js";import"./render-strategy-BXR9Yyrn.js";import"./index-DRtd-hRg.js";import"./index-B1LdDssV.js";import"./use-locale-context-CTaGYWMe.js";import"./use-event-eGuf5zsd.js";import"./collapsible-content-Yst3ur6b.js";import"./index-9MS2e6do.js";import"./index-DOljKnny.js";import"./check-DUDaO23Y.js";import"./index-DBzJ7FiP.js";import"./use-field-context-CzRTzM_q.js";import"./split-presence-props-DcTE3k_W.js";import"./use-presence-BYwEg9-n.js";import"./index-CMjQVyXy.js";import"./index-CdZdUDHv.js";import"./index-BTGRfmW-.js";import"./portal-BWW2K-Dr.js";import"./index-Dwc3-hf4.js";import"./index-lb5DGD3A.js";import"./dialog-trigger-BNZ6OjHh.js";import"./focus-trap.esm-PH5MDpJx.js";import"./chevron-left-Bqxy6nNn.js";import"./ellipsis-Bl27Sm0D.js";import"./chevron-right-CT14g65n.js";import"./index-C1tGBziM.js";import"./index-BR8t6gqn.js";const C=["elevated","outline","filled"],Wr={title:"Components/Card",component:t.Root,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{options:C,control:{type:"select"}}},args:{}},e={args:{},render:a=>r.jsxs(t.Root,{...a,children:[r.jsxs(t.Header,{children:[r.jsx(t.Title,{children:"Sign in"}),r.jsx(t.Description,{children:"Welcome! Begin your journey here."})]}),r.jsxs(t.Content,{className:"trds-flex trds-flex-col",children:[r.jsx(i,{type:"text",placeholder:"your email",className:"trds-mb-2"}),r.jsx(i,{type:"password",placeholder:"your password"})]}),r.jsxs(t.Footer,{className:"trds-flex trds-flex-row trds-gap-4",children:[r.jsx(n,{className:"trds-flex-1",children:"Sign in"}),r.jsx(n,{theme:"primary",className:"trds-flex-1",children:"🚀 Sign up"})]})]})},o={args:{},render:a=>r.jsx("div",{className:"trds-flex trds-flex-row trds-gap-4",children:C.map(s=>r.jsxs(t.Root,{variant:s,...a,children:[r.jsx(t.Header,{children:r.jsx(g,{src:"https://i.pravatar.cc/500",size:"lg"})}),r.jsx(t.Content,{className:"trds-text-neutral-500",children:"An adventurer at heart and a visionary by nature, Alex has journeyed through the unknown, bringing light to the darkest corners of the world."}),r.jsx(t.Footer,{children:r.jsx("span",{className:"trds-text-md trds-font-bold trds-text-neutral-400",children:"- Meet Alex Winters"})})]},s))})};var p,m,d,l,c;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {},
+  render: (props: CardProps) => {
+    return <Card.Root {...props}>
+        <Card.Header>
+          <Card.Title>Sign in</Card.Title>
+          <Card.Description>Welcome! Begin your journey here.</Card.Description>
+        </Card.Header>
+        <Card.Content className="trds-flex trds-flex-col">
+          <Input type="text" placeholder="your email" className="trds-mb-2" />
+          <Input type="password" placeholder="your password" />
+        </Card.Content>
+        <Card.Footer className="trds-flex trds-flex-row trds-gap-4">
+          <Button className="trds-flex-1">Sign in</Button>
+          <Button theme="primary" className="trds-flex-1">
+            🚀 Sign up
+          </Button>
+        </Card.Footer>
+      </Card.Root>;
+  }
+}`,...(d=(m=e.parameters)==null?void 0:m.docs)==null?void 0:d.source},description:{story:"TODO: 추후 Input component로 교체 필요",...(c=(l=e.parameters)==null?void 0:l.docs)==null?void 0:c.description}}};var u,x,h;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {},
+  render: (props: CardProps) => {
+    return <div className="trds-flex trds-flex-row trds-gap-4">
+        {variants.map(variant => <Card.Root variant={variant} key={variant} {...props}>
+            <Card.Header>
+              <Avatar src="https://i.pravatar.cc/500" size="lg" />
+            </Card.Header>
+            <Card.Content className="trds-text-neutral-500">
+              An adventurer at heart and a visionary by nature, Alex has
+              journeyed through the unknown, bringing light to the darkest
+              corners of the world.
+            </Card.Content>
+            <Card.Footer>
+              <span className="trds-text-md trds-font-bold trds-text-neutral-400">
+                - Meet Alex Winters
+              </span>
+            </Card.Footer>
+          </Card.Root>)}
+      </div>;
+  }
+}`,...(h=(x=o.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};const _r=["Default","Variant"];export{e as Default,o as Variant,_r as __namedExportsOrder,Wr as default};
