3dca51576de04b523d66ec14c3f95e8cb3e3b3e3
dev2820 <dev2820@users.noreply.github.com>
2025-01-13 14:26:40 +0000

    deploy: 9afd9d0eab2aaa99a4ee756c1bbebde14f94b590

---
+++ assets/_Link.stories-ChH6VJmH.js
@@ -0,0 +1,61 @@
+import{j as r}from"./jsx-runtime-qGIIFXMu.js";import{L as s}from"./Link-DNlc4jR2.js";import{c as L}from"./createLucideIcon-zijr-BYg.js";import"./index-CDs2tPxN.js";import"./tv-CIwtzMts.js";/**
+ * @license lucide-react v0.378.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=L("ExternalLink",[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]]);/**
+ * @license lucide-react v0.378.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const v=L("Link",[["path",{d:"M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71",key:"1cjeqo"}],["path",{d:"M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71",key:"19qd67"}]]),_={title:"Components/Link",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{},args:{}},t={args:{href:"https://github.com/dev2820/terra-design-system"},render:e=>r.jsx(s,{...e,children:"Github"})},n={args:{href:"https://github.com/dev2820/terra-design-system"},render:e=>r.jsxs("div",{className:"trds-flex trds-flex-col trds-gap-4",children:[r.jsx(s,{...e,theme:"default",children:"Github"}),r.jsx(s,{...e,theme:"primary",children:"Github"})]})},a={args:{href:"https://github.com/dev2820/terra-design-system"},render:e=>r.jsxs("div",{className:"trds-flex trds-flex-col trds-gap-4",children:[r.jsx(s,{...e,variant:"plain",children:"Github"}),r.jsx(s,{...e,variant:"underline",children:"Github"})]})},i={args:{href:"https://github.com/dev2820/terra-design-system",target:"_blank"},render:e=>r.jsxs("div",{className:"trds-flex trds-flex-col trds-gap-4",children:[r.jsx(s,{...e,leftIcon:r.jsx(v,{size:16}),children:"Github"}),r.jsx(s,{...e,rightIcon:r.jsx(b,{size:16}),children:"Github"})]})};var o,d,c;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    href: 'https://github.com/dev2820/terra-design-system'
+  },
+  render: (props: LinkProps) => {
+    return <Link {...props}>Github</Link>;
+  }
+}`,...(c=(d=t.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var p,h,l;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    href: 'https://github.com/dev2820/terra-design-system'
+  },
+  render: (props: LinkProps) => {
+    return <div className="trds-flex trds-flex-col trds-gap-4">
+        <Link {...props} theme="default">
+          Github
+        </Link>
+        <Link {...props} theme="primary">
+          Github
+        </Link>
+      </div>;
+  }
+}`,...(l=(h=n.parameters)==null?void 0:h.docs)==null?void 0:l.source}}};var m,u,g;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    href: 'https://github.com/dev2820/terra-design-system'
+  },
+  render: (props: LinkProps) => {
+    return <div className="trds-flex trds-flex-col trds-gap-4">
+        <Link {...props} variant="plain">
+          Github
+        </Link>
+        <Link {...props} variant="underline">
+          Github
+        </Link>
+      </div>;
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var k,x,f;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    href: 'https://github.com/dev2820/terra-design-system',
+    target: '_blank'
+  },
+  render: (props: LinkProps) => {
+    return <div className="trds-flex trds-flex-col trds-gap-4">
+        <Link {...props} leftIcon={<LinkIcon size={16} />}>
+          Github
+        </Link>
+        <Link {...props} rightIcon={<ExternalLinkIcon size={16} />}>
+          Github
+        </Link>
+      </div>;
+  }
+}`,...(f=(x=i.parameters)==null?void 0:x.docs)==null?void 0:f.source}}};const q=["Default","Theme","Variant","WithIcon"];export{t as Default,n as Theme,a as Variant,i as WithIcon,q as __namedExportsOrder,_ as default};
