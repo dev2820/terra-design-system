4c3a33481c3ffb676ab3c10353616ebaf8997a1d
dev2820 <second28200@naver.com>
2026-08-17 16:59:03 +0900

    chore: setup vitest

---
+++ packages/bison/react/vitest.config.ts
@@ -0,0 +1,1 @@
+export { default } from "@terra/vitest-config";
---
+++ packages/vitest-config/index.d.ts
@@ -0,0 +1,5 @@
+import type { ViteUserConfigExport } from "vitest/config";
+
+declare const config: ViteUserConfigExport;
+
+export default config;
